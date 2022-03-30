fba63f99ea64323f9963769f5cc0141a4b9c64c4
Drew <dlaikin3@gmail.com>
2022-03-30 18:58:43 -0400

    button test

--- aToz.php
+++ aToz.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 
-$sql = "SELECT * FROM Data.apps";
+$sql = "SELECT * FROM Data.apps ORDER BY name DESC";
 $result = $connect->query($sql);
 
 if ($result->num_rows > 0) {
