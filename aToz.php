01f8b0b86f060e50674eff114de7062f844785b4
Drew <dlaikin3@gmail.com>
2022-03-30 18:59:05 -0400

    button test

--- aToz.php
+++ aToz.php
@@ -6,7 +6,6 @@ $result = $connect->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    sort($row["name"]);
     while ($row = $result->fetch_assoc()) {
 
         echo '                <!-- app ' . $row["name"] . ' -->
