3fe859388a62e928e47b571c170c6690f56d5690
Drew <dlaikin3@gmail.com>
2022-03-31 15:10:31 -0400

    sort and filter are fully functional

--- js/index.js
+++ js/index.js
@@ -256,5 +256,21 @@ $(document).ready(function () {
             }
         })
     });
+    $('#windsort').click(function () {
+        $.ajax({
+            url: "windfilt.php",
+            method: "GET",
+            data: {},
+            success: function (data) {
+                $('#album').html(data);
+                // alert('ajax worked');
+
+            },
+            error: function (result) {
+                alert('ajax show apps error: :( ' + result);
+            }
+        })
+    });
+})
 })
 ; // end of jquery
