fb883cc6367b6aa6bc8377f427dfb5a982dce7ec
Jaden Zaleski <jadenzaleski@icloud.com>
2022-04-03 13:10:52 -0400

    Revert "fixed checkboxes"
    This reverts commit 1c94e83119a89bdde408a332b6d0257e91dc71a7.

--- addApp.php
+++ addApp.php
@@ -1,24 +1,13 @@
 <?php
 session_start();
 include 'config.php';
-
-$windows = false;
-$mac = false;
-
-if ($_POST['boxes'][0])
-    $mac = true;
-if ($_POST['boxes'][1])
-    $windows = true;
-
 if (isset($_SESSION["username"])) {
-
-    $sql = "INSERT INTO Data.requestedApps (id, firstName, lastName, name, imgPath, shortDescription, description, version, devName, apple, windows, downloadLink, createdAt)
- VALUES (DEFAULT, '" . $_SESSION['firstName'] . "', '" . $_SESSION['lastName'] . "', '" . $_POST['appName'] . "', '" . 'images/' .$_POST['fileName'] . "', '" . $_POST['shortDescription'] . "', '" . $_POST['description'] . "', '" . $_POST['version'] . "', '" . $_POST['devName'] . "',  '" . $mac . "',  '" . $windows . "',  '" . $_POST['downloadLink'] . "', DEFAULT)";
+    $sql = "INSERT INTO Data.requestedApps (id, name, imgPath, shortDescription, description, version, devName, apple, windows, downloadLink, createdAt)
+ VALUES (DEFAULT, '" . $_POST['appName'] . "', '" . 'images/' .$_POST['fileName'] . "', '" . $_POST['shortDescription'] . "', '" . $_POST['description'] . "', '" . $_POST['version'] . "', '" . $_POST['devName'] . "',  '" . $_POST['mac'] . "',  '" . $_POST['windows'] . "',  '" . $_POST['downloadLink'] . "', DEFAULT)";
     $result = $connect->query($sql);
-
     if (!$result)
         echo '$result failed: ' . $result;
-    else echo  'Yes';
+    else echo "Yes";
 
 }
 
