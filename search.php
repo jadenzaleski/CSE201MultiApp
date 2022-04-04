5242bb94862dc2927ba2090bec25cd7bc7945c8d
Drew <dlaikin3@gmail.com>
2022-04-04 16:46:24 -0400

    search test 4

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 
-$sql = "SELECT * FROM Data.apps WHERE name LIKE re% " . $_GET[0];
+$sql = "SELECT * FROM Data.apps WHERE name LIKE re% " . $_GET['chars'];
 $result = $connect->query($sql);
 
 if ($result->num_rows > 0) {
