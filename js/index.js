1b36dc8e7ba2b4a44e28651b45f604fab35480c5
Drew <dlaikin3@gmail.com>
2022-04-06 13:05:09 -0400

    Search Bar Now works

--- js/index.js
+++ js/index.js
@@ -430,7 +430,7 @@ $(document).ready(function () {
         })
     });
 //Searchbar script
-    $('#searchBar').on('keydown', function () {
+    $('#searchBar').on('keyup', function () {
         let chars = $('#searchBar').val();
         $.ajax({
             url: "search.php",
